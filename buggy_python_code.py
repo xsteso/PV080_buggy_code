8b7b42ac780ae0c810698d011402eaa1e0e5f639
Martin Šteso <xsteso@fi.muni.cz>
2023-03-28 08:48:00 +0200

    sdasdas

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,7 +25,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    eval(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
     try: 
@@ -37,7 +37,7 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream) #deserializing data
     return deserialized_data
     
 def authenticate(password):
